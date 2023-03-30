ef80e80f49ab86b9bec67caf2285fb5c256b41ee
Charanaraj-16 <craj36755@gmail.com>
2023-03-30 20:51:54 +0530

    FlightReservationProject

---
+++ FLightBookingSystem/Models/BookingModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FLightBookingSystem.Models
+{
+    public class BookingModel
+    {
+        public int BookingID;
+        [Required(ErrorMessage = "The date is required")]
+        [Display(Name = "Select Date:")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMM/yyyy}")]
+        public DateTime SelectedDate;
+
+        public SelectList BookingIDs { get; set; }
+    }
+    public class CancelationModel
+    {
+        public int BookingID;
+        public int Payment;
+    }
+}
